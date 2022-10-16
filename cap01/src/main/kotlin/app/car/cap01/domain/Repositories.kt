199bed6baac5154a31b957cafd2d5ca0d6e2f3ad
Rielly Magalhães <magalhaesrielly@gmail.com>
2022-10-16 19:47:07 -0300

    Initial commit

--- cap01/src/main/kotlin/app/car/cap01/domain/Repositories.kt
+++ cap01/src/main/kotlin/app/car/cap01/domain/Repositories.kt
@@ -3,3 +3,4 @@ package app.car.cap01.domain
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface DriverRepository : JpaRepository<Driver, Long>
+
--- cap01/src/main/kotlin/app/car/cap01/interfaces/DriverAPI.kt
+++ cap01/src/main/kotlin/app/car/cap01/interfaces/DriverAPI.kt
@@ -1,20 +1,29 @@
 package app.car.cap01.interfaces
 
 import app.car.cap01.domain.DriverRepository
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
 
 @Service
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
 class DriverAPI(
-        val driverRepository: DriverRepository
+    val driverRepository: DriverRepository
 ) {
 
     @GetMapping("/drivers")
     fun listDrivers() = driverRepository.findAll()
 
+    @GetMapping("/drivers/{id}")
+    fun findDriver(@PathVariable("id") id: Long) =
+        driverRepository.findById(id).orElseThrow {
+            ResponseStatusException(HttpStatus.NOT_FOUND)
+        }
 }
